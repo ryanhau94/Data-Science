94bdc9db513018ba108f9fc85a93241517be1870
Ryan Hou <ryanhau@outlook.com>
2021-01-07 19:41:56 -0600

    Update C# pick up

---
+++ CSharp/Basics.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Basics {
+
+    class Program {
+
+        static void Main(string[] args) {
+            // Data types
+            var s = "This is a string";
+            var d = 1.0;
+            var input = Console.Readline();
+
+            Console.WriteLine("The input is: {0}", input); // Print to screen
+        }
+    }
+}
